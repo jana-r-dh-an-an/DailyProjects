97141b65b0420ae7e9e0f989aedee9a7a1de6c7b
janardhanan <rsjana305@gmail.com>
2023-11-09 17:56:14 +0530

    Message Commit

---
+++ E-TICKET/code.js
@@ -0,0 +1,15 @@
+console.log(a);
+
+const a=10;
+// function ob(){
+//     var x=10;
+// }
+// console.log(window.a);
+// console.log(this.a);
+// console.log(a);
+// console.log(x);
+
+
+
+//null, undefined, window, this...
+
