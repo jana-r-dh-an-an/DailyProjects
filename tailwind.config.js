b1637d70032a2e913412c5f0ee5bae8229b38223
janardhanan <rsjana305@gmail.com>
2023-12-02 13:20:38 +0530

    all projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./DOUBLE_POPUPS/*.{html,js}"],
+  content: ["./PURPLE_LOGIN/*.{html,js}"],
   theme: {
     extend: {},
   },
